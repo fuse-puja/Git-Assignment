80686ff3d417be1689184d383f7feb95e7b7460d
Puja Pathak <puja.pathak@fusemachines.com>
2023-08-02 14:36:52 +0545

    Math function-sum, difference, product

---
+++ mathFunc.py
@@ -0,0 +1,20 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+num1 = 10
+num2 = 5
+
+sum = add(num1, num2)
+print("Sum= ", sum)
+
+difference = subtract(num1, num2)
+print("Difference= ", difference)
+
+product = multiply(num1, num2)
+print("Product= ", product)
